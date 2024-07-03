f06b6c60a57b1762d0c2e4013266230aa93da034
Boseong@0902 <alskdj7879@gmail.com>
2024-07-03 21:43:07 +0900

    feat: user post,get 구축

---
+++ app/routers.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .crud import *
+from .schemas import *
+from .database import get_db
+
+router = APIRouter()
+
+
+@router.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(db=db, user=user)
+
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
