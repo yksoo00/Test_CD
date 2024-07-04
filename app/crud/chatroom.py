a1e45f73be7bcbe7b58a4c18abf0fe81dd197278
Boseong@0902 <alskdj7879@gmail.com>
2024-07-04 15:33:03 +0900

    dcos: {chatroom.py} 채팅방 생성할 때 user_id 나 mentor_id가 없을 시, 404 에러코드 띄우도록 설정

--- app/crud/chatroom.py
+++ app/crud/chatroom.py
@@ -1,8 +1,17 @@
 from sqlalchemy.orm import Session
 from models import *
 from schemas import *
+from fastapi import HTTPException, status
 
 def create_chatroom(db: Session, chatroom: ChatroomCreate):
+    user = db.query(User).filter(User.id == chatroom.user_id).first()
+    mentor = db.query(Mentor).filter(Mentor.id == chatroom.mentor_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if not mentor:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Mentor not found")
+
+
     db_chatroom = Chatroom(user_id=chatroom.user_id, mentor_id=chatroom.mentor_id)
     db.add(db_chatroom)
     db.commit()
@@ -14,4 +23,5 @@ def delete_chatroom(db: Session, chatroom_id: int):
     if db_chatroom:
         db_chatroom.is_deleted = True
         db.commit()
-    return db_chatroom
+        return True
+    return False
