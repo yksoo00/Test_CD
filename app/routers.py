e1febe418c6edaee710e41a13de096f308e60a77
Boseong@0902 <alskdj7879@gmail.com>
2024-07-04 11:55:50 +0900

    docs: 생성된 chatroom모듈로 chatroom생성과 삭제 구현

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from crud import user as UserService
+from crud import chatroom as ChatroomService
 from schemas import *
 from database import get_db
 
@@ -18,3 +19,14 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+@router.post("/chatrooms/", response_model=ChatroomResponse)
+def create_chatroom(chatroom: ChatroomCreate, db: Session = Depends(get_db)):
+    return ChatroomService.create_chatroom(db=db, chatroom=chatroom)
+
+@router.delete("/chatrooms/{chatroom_id}", response_model=ChatroomResponse)
+def delete_chatroom(chatroom_id: int, db: Session = Depends(get_db)):
+    chatroom = ChatroomService.delete_chatroom(db, chatroom_id=chatroom_id)
+    if chatroom is None:
+        raise HTTPException(status_code=404, detail="Chatroom not found")
+    return chatroom
