34319455fb33c2b0027833abe9f127ef98827120
Boseong@0902 <alskdj7879@gmail.com>
2024-07-04 23:05:21 +0900

    dcos: {chatroom.py} get_chatroom객체에서 chatroom이 삭제된 기록이 있으면 아무것도 반환하지 않도록 설정 -> 삭제된 채팅방이 다시 삭제되는 것을 방지

--- app/crud/chatroom.py
+++ app/crud/chatroom.py
@@ -1,21 +1,9 @@
 from sqlalchemy.orm import Session
 from models import *
 from schemas import *
-from fastapi import HTTPException, status
 
 
 def create_chatroom(db: Session, chatroom: ChatroomCreate):
-    user = db.query(User).filter(User.id == chatroom.user_id).first()
-    mentor = db.query(Mentor).filter(Mentor.id == chatroom.mentor_id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    if not mentor:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Mentor not found"
-        )
-
     db_chatroom = Chatroom(user_id=chatroom.user_id, mentor_id=chatroom.mentor_id)
     db.add(db_chatroom)
     db.commit()
@@ -23,10 +11,14 @@ def create_chatroom(db: Session, chatroom: ChatroomCreate):
     return db_chatroom
 
 
+def get_chatroom(db: Session, chatroom_id: int):
+    db_chatroom = db.query(Chatroom).filter(Chatroom.id == chatroom_id).first()
+    if db_chatroom.is_deleted:
+        return
+    return db_chatroom
+
+
 def delete_chatroom(db: Session, chatroom_id: int):
     db_chatroom = db.query(Chatroom).filter(Chatroom.id == chatroom_id).first()
-    if db_chatroom:
-        db_chatroom.is_deleted = True
-        db.commit()
-        return True
-    return False
+    db_chatroom.is_deleted = True
+    db.commit()
