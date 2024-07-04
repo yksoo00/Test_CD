cae266239f42f89bc95a1716543c7b044225af31
Boseong@0902 <alskdj7879@gmail.com>
2024-07-04 23:03:51 +0900

    docs: {routers.py} 예외 처리를 각각의 crud모듈에서 하는 것이 아닌 routers.py에서 처리하게끔 수정

--- app/routers.py
+++ app/routers.py
@@ -24,14 +24,23 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @router.post("/chatrooms/", response_model=ChatroomResponse)
 def create_chatroom(chatroom: ChatroomCreate, db: Session = Depends(get_db)):
+    user = UserService.get_user(db, user_id=chatroom.user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    mentor = MentorService.get_mentor(db, mentor_id=chatroom.mentor_id)
+    if mentor is None:
+        raise HTTPException(status_code=404, detail="Mentor not found")
+
     return ChatroomService.create_chatroom(db=db, chatroom=chatroom)
 
 
-@router.delete("/chatrooms/ƒ{chatroom_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/chatrooms/{chatroom_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_chatroom(chatroom_id: int, db: Session = Depends(get_db)):
-    success = ChatroomService.delete_chatroom(db, chatroom_id=chatroom_id)
-    if not success:
+    chatroom = ChatroomService.get_chatroom(db, chatroom_id)
+    if not chatroom:
         raise HTTPException(status_code=404, detail="Chatroom not found")
+    ChatroomService.delete_chatroom(db, chatroom_id)
 
 
 @router.post("/mentors/", response_model=MentorResponse)
