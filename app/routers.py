b23e3f87ceae58dc1bda2c2738f668f2b3ca8240
bong-u <bongudev@gmail.com>
2024-07-08 15:06:57 +0900

    feat: 처방전 생성, 단건 조회, 전체 조회 기능 구현

--- app/routers.py
+++ app/routers.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from crud import user as UserService
 from crud import chatroom as ChatroomService
 from crud import mentor as MentorService
+from crud import prescription as PrescriptionService
 from schemas import *
 from database import get_db
 
@@ -40,6 +41,16 @@ def delete_chatroom(chatroom_id: int, db: Session = Depends(get_db)):
     chatroom = ChatroomService.get_chatroom(db, chatroom_id)
     if not chatroom:
         raise HTTPException(status_code=404, detail="Chatroom not found")
+
+    # 처방전 생성
+    PrescriptionService.create_prescription(
+        db=db,
+        chatroom_id=chatroom.id,
+        user_id=chatroom.user.id,
+        mentor_id=chatroom.mentor.id,
+    )
+
+    # 채팅방 삭제
     ChatroomService.delete_chatroom(db, chatroom_id)
 
 
@@ -52,3 +63,28 @@ def create_mentor(mentor: MentorCreate, db: Session = Depends(get_db)):
 def read_mentors(db: Session = Depends(get_db)):
     mentors = MentorService.get_mentor_all(db)
     return mentors
+
+
+@router.get("/prescriptions/{prescription_id}", response_model=PrescriptionResponse)
+def read_prescription(
+    user_id: int, prescription_id: int, db: Session = Depends(get_db)
+):
+    user = UserService.get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    prescription = PrescriptionService.get_prescription(db, prescription_id)
+    if prescription is None:
+        raise HTTPException(status_code=404, detail="Prescription not found")
+
+    if prescription.user_id != user_id:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    return prescription
+
+
+@router.get("/prescriptions", response_model=list[PrescriptionResponse])
+def read_prescriptions(user_id: int, db: Session = Depends(get_db)):
+    user = UserService.get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    prescriptions = PrescriptionService.get_prescription_all(db, user_id)
+    return prescriptions
--- app/schemas.py
+++ app/schemas.py
@@ -37,12 +37,25 @@ class MentorBase(BaseModel):
     description: str
     is_spicy: bool
 
+
 class MentorCreate(MentorBase):
     pass
 
 
 class MentorResponse(MentorBase):
     id: int
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+    class Config:
+        from_attributes = True
+
+
+class PrescriptionResponse(BaseModel):
+    id: int
+    user_id: int
+    mentor_id: int
+    content: str
     created_at: datetime.datetime
     updated_at: datetime.datetime
 
