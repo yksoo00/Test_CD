06d86512147063a471fa300ad038fc0a93cf7c32
bong-u <bongudev@gmail.com>
2024-07-09 00:52:52 +0900

    style: 누락된 포맷팅 적용

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from settings import settings
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(
     settings.DATABASE_URL,
