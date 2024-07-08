4af74762ed71a92a614af08adcc8201d31c5e137
bong-u <bongudev@gmail.com>
2024-07-08 15:23:00 +0900

    comment: API 관련 주석 추가

--- app/routers.py
+++ app/routers.py
@@ -10,11 +10,13 @@ from database import get_db
 router = APIRouter()
 
 
+# 새로운 사용자를 생성하는 API
 @router.post("/users", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return UserService.create_user(db=db, user=user)
 
 
+# 사용자 목록을 조회하는 API
 @router.get("/users/{user_id}", response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = UserService.get_user(db, user_id=user_id)
@@ -23,6 +25,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 
+# 새로운 채팅방을 생성하는 API
 @router.post("/chatrooms", response_model=ChatroomResponse)
 def create_chatroom(chatroom: ChatroomCreate, db: Session = Depends(get_db)):
     user = UserService.get_user(db, user_id=chatroom.user_id)
@@ -36,6 +39,7 @@ def create_chatroom(chatroom: ChatroomCreate, db: Session = Depends(get_db)):
     return ChatroomService.create_chatroom(db=db, chatroom=chatroom)
 
 
+# 채팅방 목록을 조회하는 API
 @router.delete("/chatrooms/{chatroom_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_chatroom(chatroom_id: int, db: Session = Depends(get_db)):
     chatroom = ChatroomService.get_chatroom(db, chatroom_id)
@@ -54,17 +58,20 @@ def delete_chatroom(chatroom_id: int, db: Session = Depends(get_db)):
     ChatroomService.delete_chatroom(db, chatroom_id)
 
 
+# 멘토 생성
 @router.post("/mentors", response_model=MentorResponse)
 def create_mentor(mentor: MentorCreate, db: Session = Depends(get_db)):
     return MentorService.create_mentor(db=db, mentor=mentor)
 
 
+# 멘토 목록 조회
 @router.get("/mentors", response_model=list[MentorResponse])
 def read_mentors(db: Session = Depends(get_db)):
     mentors = MentorService.get_mentor_all(db)
     return mentors
 
 
+# 처방전 조회
 @router.get("/prescriptions/{prescription_id}", response_model=PrescriptionResponse)
 def read_prescription(
     user_id: int, prescription_id: int, db: Session = Depends(get_db)
@@ -76,11 +83,13 @@ def read_prescription(
     if prescription is None:
         raise HTTPException(status_code=404, detail="Prescription not found")
 
+    # 처방전을 조회하는 사용자와 처방전의 사용자가 동일한지 확인
     if prescription.user_id != user_id:
         raise HTTPException(status_code=403, detail="Forbidden")
     return prescription
 
 
+# 처방전 목록 조회
 @router.get("/prescriptions", response_model=list[PrescriptionResponse])
 def read_prescriptions(user_id: int, db: Session = Depends(get_db)):
     user = UserService.get_user(db, user_id=user_id)
