d0d5f0878a3b3d8cf3bc66ec3ec747ddc325b329
Boseong@0902 <alskdj7879@gmail.com>
2024-07-10 20:35:00 +0900

    chore : 환경변수 수정

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,12 @@
 import os
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DATABASE_URL: str
 
     class Config:
-        env_file = f".env.
-        {os.getenv('ENV', 'development')}"
+        env_file = f".env.{os.getenv('ENV', 'development')}"
+
 
 settings = Settings()
