3776244a0b23fc517647581345733366030d16cc
Boseong@0902 <alskdj7879@gmail.com>
2024-07-10 17:41:57 +0900

    refactor : class config 수정

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,11 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
 
     class Config:
-        env_file = ".env"
+        env_file = f".env.
+        {os.getenv('ENV', 'development')}"
 
 settings = Settings()
