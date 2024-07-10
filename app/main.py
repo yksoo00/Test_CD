908d7f3639cdd10ed03079d7234960c45cd96aae
bong-u <bongudev@gmail.com>
2024-07-10 13:58:06 +0900

    feat: CORS 추가 localhost:5173 127.0.0.1:5173

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
-import uvicorn
 from fastapi import FastAPI
 from database import Base, engine
-from models import *
 from routers import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(user_router, prefix="/api")
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-# if __name__ == "__main__":
-#     uvicorn.run("main", host="0.0.0.0", port=8000, reload=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user_router, prefix="/api")
