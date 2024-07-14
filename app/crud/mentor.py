5a111de172084414b63b24e8ed13c4cddda606fe
Boseong@0902 <alskdj7879@gmail.com>
2024-07-15 01:06:51 +0900

    comment : 주석 수정

--- app/crud/mentor.py
+++ app/crud/mentor.py
@@ -3,7 +3,7 @@ from models import Mentor
 from schemas import MentorCreate
 
 
-# 멘토 인스턴스 생성 후 DB에 추가해주는 객체
+# 멘토 인스턴스 생성 후 DB에 추가해주는 함수
 def create_mentor(db: Session, mentor: MentorCreate):
     db_mentor = Mentor(name=mentor.name, description=mentor.description)
     db.add(db_mentor)
@@ -12,11 +12,11 @@ def create_mentor(db: Session, mentor: MentorCreate):
     return db_mentor
 
 
-# DB에서 멘토 불러오는 객체
+# DB에서 멘토 불러오는 함수
 def get_mentor(db: Session, mentor_id: int):
     return db.query(Mentor).filter(Mentor.id == mentor_id).first()
 
 
-# DB에서 모든 멘토 불러오는 객체
+# DB에서 모든 멘토 불러오는 함수
 def get_mentor_all(db: Session):
     return db.query(Mentor).all()
