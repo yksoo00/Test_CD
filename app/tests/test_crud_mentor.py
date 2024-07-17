ae4dba08da05284988545ce77c25f42bdf8803e6
bong-u <bongudev@gmail.com>
2024-07-17 20:29:11 +0900

    test: get_mentor, get_mentor_all 테스트 추가

--- app/tests/test_crud_mentor.py
+++ app/tests/test_crud_mentor.py
@@ -1,5 +1,5 @@
 import unittest
-from test_base import SessionLocal
+from test_base import SessionLocal, clear_db
 from crud.mentor import *
 
 
@@ -12,6 +12,8 @@ class TestMentor(unittest.TestCase):
 
     # 테스트 종료 후에 실행
     def tearDown(self):
+        # DB 초기화
+        clear_db()
         # 세션 종료
         self.db.close()
 
@@ -26,3 +28,30 @@ class TestMentor(unittest.TestCase):
 
         assert db_mentor.name == test_name
         assert db_mentor.description == test_description
+
+    def test_get_mentor(self):
+        test_name = "test_name"
+        test_description = "test_description"
+        test_mentor = Mentor(name=test_name, description=test_description)
+        self.db.add(test_mentor)
+        self.db.commit()
+        self.db.refresh(test_mentor)
+
+        db_mentor = get_mentor(self.db, test_mentor.id)
+
+        assert db_mentor.name == test_name
+        assert db_mentor.description == test_description
+
+    def test_get_mentor_all(self):
+        test_name = "test_name"
+        test_description = "test_description"
+        test_mentor = Mentor(name=test_name, description=test_description)
+        self.db.add(test_mentor)
+        self.db.commit()
+        self.db.refresh(test_mentor)
+
+        db_mentors = get_mentor_all(self.db)
+
+        assert len(db_mentors) == 1
+        assert db_mentors[0].name == test_name
+        assert db_mentors[0].description == test_description
