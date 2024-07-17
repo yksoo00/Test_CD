636d89ac2d65ae274c5406cde8bad6cf4287eac2
bong-u <bongudev@gmail.com>
2024-07-17 14:29:18 +0900

    test: create_mentor 테스트 추가

---
+++ app/tests/test_crud_mentor.py
@@ -0,0 +1,28 @@
+import unittest
+from test_base import SessionLocal
+from crud.mentor import *
+
+
+class TestMentor(unittest.TestCase):
+
+    # 테스트 시작 전에 실행
+    def setUp(self):
+        # 세션 생성
+        self.db = SessionLocal()
+
+    # 테스트 종료 후에 실행
+    def tearDown(self):
+        # 세션 종료
+        self.db.close()
+
+    def test_create_mentor(self):
+        test_name = "test_name"
+        test_description = "test_description"
+        test_mentor = create_mentor(
+            self.db, MentorCreate(name=test_name, description=test_description)
+        )
+
+        db_mentor = self.db.query(Mentor).filter(Mentor.id == test_mentor.id).first()
+
+        assert db_mentor.name == test_name
+        assert db_mentor.description == test_description
