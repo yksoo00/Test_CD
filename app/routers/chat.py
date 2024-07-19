05d550b5e7c8249a0f5ee5d23294c8328def7efd
Boseong@0902 <alskdj7879@gmail.com>
2024-07-19 15:43:06 +0900

    refactor : 원래 목소리로 나오도록 코드 변경

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -16,8 +16,6 @@ import json
 
 router = APIRouter()
 
-voice_model_list = ("ko-KR-InJoonNeural", "ko-KR-SunHiNeural", "ko-KR-HyunsuNeural")
-
 
 def generate_gpt_payload(chat_memory_messages, prompt):
     # 기존 대화 기록 추가
@@ -99,7 +97,7 @@ async def websocket_endpoint(
 
             # 음성을 생성하는 celery task 실행
             server_audio = celery_worker.generate_audio_from_string.delay(
-                trim_text(gpt_answer), voice_model_list[chatroom.mentor_id - 1]
+                trim_text(gpt_answer)
             ).get()
 
             # GPT의 답변과 음성을 클라이언트에게 전송
