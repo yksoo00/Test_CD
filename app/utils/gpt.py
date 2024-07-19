0b5a93c81888f04a632d606ffb1f78d7f04579ee
yksoo00 <yksoo00@naver.com>
2024-07-19 17:14:16 +0900

    feat:gpt 응답시 단어 반복, 창의성 줄이기 추가

--- app/utils/gpt.py
+++ app/utils/gpt.py
@@ -9,5 +9,10 @@ client = OpenAI(
 
 
 def get_gpt_answer(gpt_payload):
-    gpt_response = client.chat.completions.create(model=GPT_MODEL, messages=gpt_payload)
+    gpt_response = client.chat.completions.create(
+        model=GPT_MODEL,
+        messages=gpt_payload,
+        temperature=0.7,
+        frequency_penalty=0.2,
+    )
     return gpt_response.choices[0].message.content.strip()
