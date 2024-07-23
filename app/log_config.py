4575c2dc0c0771401354438d596b20ec4b4a4714
bong-u <bongudev@gmail.com>
2024-07-23 16:46:33 +0900

    fix: 테스트할때 setup_logging이 실행되지 않도록 수정

--- app/log_config.py
+++ app/log_config.py
@@ -1,8 +1,9 @@
-import logging
+import os, logging
 import logging.config
 from formatter import ColoredFormatter
 
-def setup_logging(config_file='log.ini'):
+
+def setup_logging(config_file="log.ini"):
     logging.config.fileConfig(config_file)
 
     # 루트 로거 가져오기
@@ -17,7 +18,13 @@ def setup_logging(config_file='log.ini'):
 
     # 사용자 정의 포매터 설정
     if console_handler:
-        console_handler.setFormatter(ColoredFormatter('%(levelname)-5s - %(asctime)s - %(funcName)s - %(message)s'))
+        console_handler.setFormatter(
+            ColoredFormatter(
+                "%(levelname)-5s - %(asctime)s - %(funcName)s - %(message)s"
+            )
+        )
+
 
 # setup_logging을 호출하여 로깅 설정 적용
-setup_logging()
+if os.environ.get("TESTING") != "True":
+    setup_logging()
