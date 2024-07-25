26bb78142bc7720c08e69c06bd8f54fe64e65138
Boseong@0902 <alskdj7879@gmail.com>
2024-07-26 00:16:53 +0900

    test : client생성, user_id, mentor_id 반환 fixture 생성

--- app/tests/test_api_mentor.py
+++ app/tests/test_api_mentor.py
@@ -1,31 +1,27 @@
-import unittest
-from conftest import client
+import pytest
 from database import clear_db
 
+@pytest.fixture(scope="function", autouse=True)
+def clear_tests():
+    yield
+    clear_db()
 
-class TestMentorApi(unittest.TestCase):
-
-    def tearDown(self):
-        clear_db()
-
-
-    def test_read_mentors(self):
-        num_of_mentors = 3
-        for _ in range(num_of_mentors):
-            test_mentor_name = "test_mentor"
-            response = client.post(
-                "/api/mentors",
-                json={"name": test_mentor_name, "description": "test mentor description"},
-            )
-
-        response = client.get(f"/api/mentors")
+def test_read_mentors(client):
+    num_of_mentors = 3
+    for _ in range(num_of_mentors):
+        test_mentor_name = "test_mentor"
+        response = client.post(
+            "/api/mentors",
+            json={"name": test_mentor_name, "description": "test mentor description"},
+        )
         assert response.status_code == 200
-        mentors = response.json()
-        assert len(mentors) == num_of_mentors
 
-    def test_read_mentor_not_found(self):
-        test_mentor_id = 1
-        response = client.get(f"/api/mentors/{test_mentor_id}")
-        assert  response.status_code == 404
+    response = client.get("/api/mentors")
+    assert response.status_code == 200
+    mentors = response.json()
+    assert len(mentors) == num_of_mentors
 
-    
+def test_read_mentor_not_found(client):
+    test_mentor_id = 1
+    response = client.get(f"/api/mentors/{test_mentor_id}")
+    assert response.status_code == 404
--- app/tests/test_api_user.py
+++ app/tests/test_api_user.py
@@ -1,60 +1,43 @@
-import unittest
-
-from conftest import client
+import pytest
 from database import clear_db
 
+@pytest.fixture(scope="function", autouse=True)
+def clear_tests():
+    yield
+    clear_db()
 
-class TestUserApi(unittest.TestCase):
+def test_create_user(client):
+    test_nickname = "test_nickname"
+    response = client.post(
+        "/api/users",
+        json={"nickname": test_nickname},
+    )
+    assert response.status_code == 200
+    assert response.json()["nickname"] == test_nickname
 
-    def tearDown(self):
-        clear_db()
+def test_read_user(client, user_id):
+    response = client.get(f"/api/users/{user_id}")
+    assert response.status_code == 200
+    assert response.json()["nickname"] == "test_nickname"
 
-    def test_create_user(self):
-        test_nickname = "test_nickname"
-        response = client.post(
-            "/api/users",
-            json={"nickname": test_nickname},
-        )
-        assert response.status_code == 200
-        assert response.json()["nickname"] == test_nickname
+def test_read_user_not_found(client):
+    test_user_id = 1
+    response = client.get(f"/api/users/{test_user_id}")
+    assert response.status_code == 404
 
-    def test_read_user(self):
-        test_nickname = "test_nickname"
-        response = client.post(
-            "/api/users",
-            json={"nickname": test_nickname},
-        )
-        user_id = response.json()["id"]
+def test_modify_user(client, user_id):
+    new_nickname = "new_nickname"
+    response = client.put(
+        f"/api/users/{user_id}",
+        json={"nickname": new_nickname},
+    )
+    assert response.status_code == 200
+    assert response.json()["nickname"] == new_nickname
 
-        response = client.get(f"/api/users/{user_id}")
-        assert response.status_code == 200
-        assert response.json()["nickname"] == test_nickname
-
-    def test_read_user_not_found(self):
-        test_user_id = 1
-        response = client.get(f"/api/users/{test_user_id}")
-        assert response.status_code == 404
-
-    def test_modify_user(self):
-        test_nickname = "test_nickname"
-        response = client.post(
-            "/api/users",
-            json={"nickname": test_nickname},
-        )
-        user_id = response.json()["id"]
-
-        new_nickname = "new_nickname"
-        response = client.put(
-            f"/api/users/{user_id}",
-            json={"nickname": new_nickname},
-        )
-        assert response.status_code == 200
-        assert response.json()["nickname"] == new_nickname
-
-    def test_modify_user_not_found(self):
-        test_user_id = 1
-        response = client.put(
-            f"/api/users/{test_user_id}",
-            json={"nickname": "new_nickname"},
-        )
-        assert response.status_code == 404
+def test_modify_user_not_found(client):
+    test_user_id = 1
+    response = client.put(
+        f"/api/users/{test_user_id}",
+        json={"nickname": "new_nickname"},
+    )
+    assert response.status_code == 404
